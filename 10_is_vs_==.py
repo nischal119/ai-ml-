50bf9cfcf2ec5e6b239abdc3f544796ac994725a
Nischal <dhungeln12@gmail.com>
2024-04-06 18:44:15 +0545

    up to equality

---
+++ 10_is_vs_==.py
@@ -0,0 +1,5 @@
+# == compares the general value but
+# is compares the whether ta values are in same memory location or not
+
+print(True == 1)  # This will print true but
+print(True is 1)  # This will print false
